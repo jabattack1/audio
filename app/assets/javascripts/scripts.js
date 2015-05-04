5682068043ef692052255f067bc1d6e437fc6992
jabattack1 <peter.hahn@live.com>
2015-05-03 20:22:16 -0400

    MV

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -6,7 +6,7 @@ var logo ="<a href=\"/\" class='animated fadeIn' onClick=\"home();return false;\
 var AudioBoyInfo ="<div id='gel'><h2 id='audiopeter' class='animated fadeIn'>Audio Boy = Rebel Solo</h2><h2 id='audiodo' class='animated fadeIn'>Audio Boy = Peter Hahn</h2>"
 var PeterHahnInfo ="<div id='gel2'><h2 id='peterdo' class='animated fadeIn'>Peter Hahn = Songwriter/Producer/<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSinger/Musician</h2></div>"
 var ContactInfo ="<div id='gel4'><h2 id='contactpeter' class='animated fadeIn'>551 689 3517</h2></div>"
-var MV ="<div id='gel3'><iframe class='animated slideInUp' width=\"660\" height=\"415\" src=\"https://www.youtube.com/embed/B_HmvMoca3c\" frameborder=\"0\" allowfullscreen></iframe></div>"
+var MV ="<iframe class='animated slideInUp' width=\"660\" height=\"415\" src=\"https://www.youtube.com/embed/etAH9IhStBc\" frameborder=\"0\" allowfullscreen></iframe>"
 var headshot = "<img src='http://i1301.photobucket.com/albums/ag119/jabattack1/phat_zpsnq9fh8qf.jpg' class='animated fadeIn' id='photo'>";
 var RebelSolo ="<img src='http://i1301.photobucket.com/albums/ag119/jabattack1/rebel%20solo%20BG_zps3xicpxkh.jpg' class='animated fadeIn' id='RebelSolo'>";
 var stuckgraph ="<img src='http://i1301.photobucket.com/albums/ag119/jabattack1/soundWaveGIF_zpshjfeknmg.gif' class='animated fadeIn' id='stuckgraph'>";
